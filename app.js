87c7c596d0d995bb136ffcdc7215ae2225884f6d
Ketzel Velásquez <zs24013346@estudiantes.uv.mx>
2026-09-15 18:49:40 -0600

    inicio de sesión

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const productoRoutes = require("./routes/productoRoutes");
 // const empleadoRoute
 const empleadoRoutes = require("./routes/empleadoRoutes");
 
-
+const path = require('path');
 
 const app = express();
 /**
@@ -27,6 +27,9 @@ app.use(express.json());
  * Middleware para servir archivos estáticos (frontend UI, assets, etc.).
  */
 app.use(express.static("public"));
+
+
+app.use('/data', express.static(path.join(__dirname, 'data')));
 
 // app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/productos", productoRoutes);
